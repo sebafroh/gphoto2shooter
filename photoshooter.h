2936b8887a45e3b523747c2680f8d352e22810f3
sebafroh <unrath@unterderbruecke.de>
2015-01-08 20:23:51 +0100

    Create photoshooter.h

---
+++ photoshooter.h
@@ -0,0 +1,90 @@
+#ifndef IMAGEVIEWER_H
+#define IMAGEVIEWER_H
+
+#include <QMainWindow>
+
+#include <QPushButton>
+#include <QGridLayout>
+#include <QWidget>
+#include <QSpinBox>
+#include <QSpacerItem>
+#include <QThread>
+#include <QProcess>
+#include <QByteArray>
+
+class QAction;
+class QLabel;
+class QMenu;
+class QScrollArea;
+class QScrollBar;
+
+class PhotoShooter : public QMainWindow
+{
+    Q_OBJECT
+
+    class Shooter: public QThread {
+
+    public:
+        Shooter(QObject* parentO, PhotoShooter* parent);
+        ~Shooter();
+
+    protected:
+        void run() Q_DECL_OVERRIDE;
+
+    private:
+        PhotoShooter* parent;
+
+    };
+
+public:
+    PhotoShooter();
+    ~PhotoShooter();
+    bool loadFile(const QString &);
+
+private slots:
+    void showCamera();
+    void singleShot();
+    void multipleShot();
+    void selectFolder();
+    void zoomIn();
+    void zoomOut();
+    void normalSize();
+    void fitToWindow();
+    void about();
+
+private:
+    void createActions();
+    void createMenus();
+    void updateActions();
+    void scaleImage(double factor);
+    void adjustScrollBar(QScrollBar *scrollBar, double factor);
+    int  takePicture();
+
+    QLabel *imageLabel;
+    QScrollArea *scrollArea;
+    double scaleFactor;
+    bool running;
+    Shooter* shooter;
+    QString mydir;
+
+    QLabel* cheese;
+    QSpinBox* timeintervall;
+    QPushButton* multipleButton;
+
+    QAction *exitAct;
+    QAction *singleShotAct;
+    QAction *multipleShotAct;
+    QAction *showCameraAct;
+    QAction *selectFolderAct;
+    QAction *zoomInAct;
+    QAction *zoomOutAct;
+    QAction *normalSizeAct;
+    QAction *fitToWindowAct;
+    QAction *aboutAct;
+
+    QMenu *fileMenu;
+    QMenu *viewMenu;
+    QMenu *helpMenu;
+};
+
+#endif
